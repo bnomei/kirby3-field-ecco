86009a96e5bdc31d9a79a2c25efe7d1799b0086c
Bruno Meilick <bnomei@users.noreply.github.com>
2020-02-07 19:14:48 +0000

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 Kirby::plugin('bnomei/field-ecco', [
     'fieldMethods' => [
-        'ecco' => function ($field, string $a, string $b): string {
+        'ecco' => function ($field, string $a, string $b = ''): string {
             return $field->bool() ? $a : $b;
         }
     ]
